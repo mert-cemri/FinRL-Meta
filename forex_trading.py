3f72adfc08949cc86180d78f237f0999ce8e8474
mert-cemri <79153625+mert-cemri@users.noreply.github.com>
2023-11-29 12:48:17 -0800

    Add files via upload

--- forex_trading.py
+++ forex_trading.py
@@ -12,8 +12,8 @@ import sys
 import time
 import datetime
 sys.path.append("../FinRL-Library")
-#os.chdir('FinRL-Meta')
-#sys.path.insert(1, '/Users/mertcemri/Desktop/my_scripts/cs285/project/FinRL-Meta')
+os.chdir('FinRL-Meta')
+sys.path.insert(1, '/Users/mertcemri/Desktop/my_scripts/cs285/project/FinRL-Meta')
 from meta import config
 from meta.data_processor import DataProcessor
 from meta.env_fx_trading.env_fx import tgym
@@ -27,7 +27,7 @@ config.TIME_ZONE_SELFDEFINED = 'US/Eastern'
 
 dp = DataProcessor(data_source="yahoofinance",
                    start_date = '2017-01-01',
-                   end_date = '2018-01-01',
+                   end_date = '2021-01-01',
                    time_interval='1D')
 
 symbol="GBPUSD"
@@ -75,11 +75,30 @@ def train(env, agent, df, if_vix = True,**kwargs):
             model = A2C("MlpPolicy", env_train,learning_rate=learning_rate,
                         n_steps=2048, ent_coef=0.0,
                         gamma=gamma, seed=seed)    
-            
         elif agent =='ddpg':
             model = DDPG("MlpPolicy", env_train, learning_rate=learning_rate,
-                        gradient_steps=10,batch_size=batch_size,
+                        gradient_steps=10,batch_size=batch_size, seed=seed)
+            
+        elif agent == "multiagent_implementation_1":
+            model1 = PPO("MlpPolicy", env_train, learning_rate=learning_rate,
+                        n_steps=2048, batch_size=batch_size, ent_coef=0.0,
                         gamma=gamma, seed=seed)
+            
+            action1_list = []
+            reward_list = []
+            while not done:
+                action, _states = model.predict(obs)
+                obs, rewards, done, info = t.step(action)
+                #print(info["Close"])
+                action1_list.append(action)
+                reward_list.append(rewards)
+            
+            ##alter df or observation space
+            env_train_2 = DummyVecEnv([lambda:env(df)])
+            model2 = PPO("MlpPolicy", env_train_2, learning_rate=learning_rate,
+                        n_steps=2048, batch_size=batch_size, ent_coef=0.0,
+                        gamma=gamma, seed=seed)
+
 
         start_time = time.time()
         s = datetime.datetime.now()
@@ -87,7 +106,7 @@ def train(env, agent, df, if_vix = True,**kwargs):
         print(f'Training start: {s}')
         model.learn(total_timesteps=total_timesteps, tb_log_name = agent)
         print('Training finished!')
-        model_name = f"./data/models/{symbol}-week-" + s.strftime('%Y%m%d%H%M%S')
+        model_name = f"./data/models/{symbol}-{agent}-week-" + s.strftime('%Y%m%d%H%M%S')
         model.save(model_name)
         print(f'Trained model saved in {model_name}')
         print(f"trainning time: {(time.time() - start_time)}")
@@ -99,15 +118,17 @@ df.rename(columns={"open": "Open", "high": "High", "low": "Low", "close": "Close
 
 train(env=tgym,agent="ddpg",df=df)
 
-#assert False
+assert False
+
 
 # # if model: del model # remove to demonstrate saving and loading
-# # PPO Models: ./data/models/GBPUSD-week-20231126131448, ./data/models/GBPUSD-week-20231123194718.zip
-# # A2C Models: ./data/models/GBPUSD-week-20231126185131, ./data/models/GBPUSD-week-20231126205306
-# # 
+# # PPO Models: ./data/models/GBPUSD-ppo-week-20231129120408 ./data/models/GBPUSD-week-20231126131448, ./data/models/GBPUSD-week-20231123194718.zip
+# # A2C Models: ./data/models/GBPUSD-a2c-week-20231129121005 ./data/models/GBPUSD-week-20231126185131, ./data/models/GBPUSD-week-20231126205306
+# # DDPG Models: ./data/models/GBPUSD-week-20231129114537
 print("we are predicting now")
 
 symbol="GBPUSD"
+
 
 dp = DataProcessor(data_source="yahoofinance",
                    start_date = '2017-01-01',
@@ -129,10 +150,11 @@ df['month'] = df['dt'].dt.month
 df['year'] = df['dt'].dt.year
 df['day'] = df['dt'].dt.day
 
+
 df.rename(columns={"open": "Open", "high": "High", "low": "Low", "close": "Close"}, inplace=True)
 t = tgym(df)
 
-model_name=f'./data/models/{symbol}-week-20231123194718.zip'
+model_name=f'./data/models/GBPUSD-ppo-week-20231129120408.zip'
 model = PPO.load(model_name)
 print("model loaded")
 
@@ -148,12 +170,35 @@ while not done:
     obs, rewards, done, info = t.step(action)
     #print(info["Close"])
     balance_list.append(t.balance)
-    #reward_list.append(rewards)
+    reward_list.append(rewards)
+
+reward_list_ppo = reward_list
+balance_list_ppo = balance_list
+
+model_name=f'./data/models/GBPUSD-week-20231129114537.zip'
+model = DDPG.load(model_name)
+print("model loaded")
+
+start_time = time.time()
+obs = t.reset()
+t.current_step=0
+done = False
+
+reward_list = []
+balance_list = []
+while not done:
+    action, _states = model.predict(obs)
+    obs, rewards, done, info = t.step(action)
+    #print(info["Close"])
+    balance_list.append(t.balance)
+    reward_list.append(rewards)
     #t.render(mode='graph')
 
 plt.figure()
 print(os.getcwd())
+#plt.plot(reward_list_ppo)
 plt.plot(balance_list)
-plt.savefig(f'trial_figures/forex_{symbol}_balance_a2c.jpg')
+#plt.legend(["ppo", "a2c"])
+plt.savefig(f'trial_figures/forex_{symbol}_balance_ddpg.jpg')
 print(f"--- running time: {(time.time() - start_time)}---")
 
